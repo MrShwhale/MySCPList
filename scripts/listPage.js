b23b3fee0394145a61c9779b76e9ff07efee1e8f
MrShwhale <wapatmore@gmail.com>
2023-09-01 12:50:03 -0700

    Add reading list support

--- scripts/listPage.js
+++ scripts/listPage.js
@@ -10,59 +10,83 @@ const toggleRow = (element) => {
   // console.log(event);
 }
 
-// For now, just work with the one list
-chrome.storage.local.get("completed").then((data) => {
-    // Entry format: [url, title, authors, rating, notes, enteredTime, updatedTime]
-    let entries = "completed" in data ? data["completed"] : [];
-
-    const listTable = document.getElementById("list-table");
-
-    for (let i = 0; i < entries.length; i++) {
-        // Make an element out of this entry
-        // Add the row to the table
-        const entryRow = listTable.insertRow(-1);
-
-        // Page title which is linked to the page
-        const pageTitleCell = entryRow.insertCell(0);
-        const pageTitle = document.createElement("p");
-        const pageLink = document.createElement("a");
-        pageLink.setAttribute("href", SCP_URL + entries[i][0]);
-        pageLink.innerHTML = entries[i][1];
-        pageTitle.appendChild(pageLink);
-        pageTitleCell.appendChild(pageTitle);
-
-        // Authors
-        const authorsCell = entryRow.insertCell(1);
-        const authors = document.createElement("p");
-        authors.innerHTML = entries[i][2].join(", ");
-        authorsCell.appendChild(authors);
-
-        // Rating
-        const ratingCell = entryRow.insertCell(2);
-        const rating = document.createElement("p");
-        rating.innerHTML = entries[i][3].toString();
-        ratingCell.appendChild(rating);
-
-        // Entered time
-        const enteredTimeCell = entryRow.insertCell(3);
-        const enteredTime = document.createElement("p");
-        enteredTime.innerHTML = new Date(entries[i][5]).toLocaleString();
-        enteredTimeCell.appendChild(enteredTime);
-
-        // Updated time
-        const updatedTimeCell = entryRow.insertCell(4);
-        const updatedTime = document.createElement("p");
-        updatedTime.innerHTML = new Date(entries[i][6]).toLocaleString();
-        updatedTimeCell.appendChild(updatedTime);
-
-        // Notes (hidden)
-        const notesCell = entryRow.insertCell(5);
-        const notes = document.createElement("p");
-        notes.innerHTML = entries[i][4];
-        notesCell.appendChild(notes);
-        notesCell.classList.add("expanded-row-content");
-        notesCell.classList.add("hide-row");
-
-        entryRow.addEventListener("click", toggleRow);
+function clearList() {
+    const table = document.getElementById("list-table");
+    while (table.firstChild != table.lastChild) {
+        table.removeChild(table.lastChild);
     }
-});
+}
+
+function displayList(listName) {
+    // For now, just work with the one list
+    chrome.storage.local.get(listName).then((data) => {
+        // Completed entry format: [url, title, authors, rating, notes, enteredTime, updatedTime]
+        let entries = listName in data ? data[listName] : [];
+
+        const listTable = document.getElementById("list-table");
+
+        for (let i = 0; i < entries.length; i++) {
+            // Make an element out of this entry
+            // Add the row to the table
+            const entryRow = listTable.insertRow(-1);
+
+            // Page title which is linked to the page
+            const pageTitleCell = entryRow.insertCell(0);
+            const pageTitle = document.createElement("p");
+            const pageLink = document.createElement("a");
+            pageLink.setAttribute("href", SCP_URL + entries[i][0]);
+            pageLink.innerHTML = entries[i][1];
+            pageTitle.appendChild(pageLink);
+            pageTitleCell.appendChild(pageTitle);
+
+            // Authors
+            const authorsCell = entryRow.insertCell(1);
+            const authors = document.createElement("p");
+            authors.innerHTML = entries[i][2].join(", ");
+            authorsCell.appendChild(authors);
+
+            // Rating
+            const ratingCell = entryRow.insertCell(2);
+            const rating = document.createElement("p");
+            rating.innerHTML = rating ? entries[i][3].toString() : "unread";
+            ratingCell.appendChild(rating);
+
+            // Entered time
+            const enteredTimeCell = entryRow.insertCell(3);
+            const enteredTime = document.createElement("p");
+            enteredTime.innerHTML = new Date(entries[i][5]).toLocaleString();
+            enteredTimeCell.appendChild(enteredTime);
+
+            // Updated time
+            const updatedTimeCell = entryRow.insertCell(4);
+            const updatedTime = document.createElement("p");
+            updatedTime.innerHTML = new Date(entries[i][6]).toLocaleString();
+            updatedTimeCell.appendChild(updatedTime);
+
+            // Notes (hidden)
+            const notesCell = entryRow.insertCell(5);
+            const notes = document.createElement("p");
+            notes.innerHTML = entries[i][4];
+            notesCell.appendChild(notes);
+            notesCell.classList.add("expanded-row-content");
+            notesCell.classList.add("hide-row");
+
+            entryRow.addEventListener("click", toggleRow);
+        }
+    });
+}
+
+function addButtonListeners() {
+    // for every button in the listbuttons thing, add an event to display that list
+    for (const child of document.getElementsByClassName("list-buttons")[0].children) {
+        child.addEventListener("click", (element) => {
+            document.getElementsByClassName("selected-list")[0].classList.remove("selected-list");
+            element.target.classList.add("selected-list");
+            clearList();
+            displayList(element.target.getAttribute("id"));
+        })
+    }
+}
+
+addButtonListeners();
+displayList("completed");
--- scripts/popup.js
+++ scripts/popup.js
@@ -51,8 +51,8 @@ function openInNewTab(url) {
 }
 
 // Marks the entry as read, or updates an existing entry. 
-// Returns true if the entry was already in the list.
-function markRead(url, title, authors, rating, notes, time) {
+// If rating is false, then it adds it to the "to read" list instead
+function markRead(listName, url, title, authors, rating, notes, time) {
   
   console.log("URL: " + url);
   console.log("Title: " + title);
@@ -63,14 +63,21 @@ function markRead(url, title, authors, rating, notes, time) {
   
 
   // Entry format: [url, title, authors, rating, notes, enteredTime, updatedTime]
+  // readinglist is the same, but no rating
   // Store this data in the proper storage place (start with local for now)
-  chrome.storage.local.get("completed").then((data) => {
+  chrome.storage.local.get(null).then((data) => {
     // If there are no entries, then instead of using existing entries use a blank list
-    let entries = "completed" in data ? data["completed"] : [];
+    let entries = listName in data ? data[listName] : [];
     let newEntry = [url, title, authors, rating, notes, time, time];
     
     // Check if there is already an entry made, then add/update it
     const existingIndex = entries.findIndex((element) => {return element[0] == newEntry[0]})
+
+    // Remove this from the other list if it exists in it
+    const otherListName = listName == "completed" ? "toRead" : "completed";
+    let otherEntries = otherListName in data ? data[otherListName] : [];
+    const newOther = otherEntries.filter((element) => {return element[0] != url});
+    chrome.storage.local.set({[otherListName]: newOther});
 
     if (existingIndex == -1) {
       entries.push(newEntry);
@@ -81,8 +88,48 @@ function markRead(url, title, authors, rating, notes, time) {
       entries[existingIndex] = newEntry;
     }
 
-    chrome.storage.local.set({"completed": entries});
+    chrome.storage.local.set({[listName]: entries});
   });
+}
+
+async function addEntry() {
+    // Get the rating and review from the user, and the author, date, title and url from the site
+
+    let entryStatus = undefined;
+    let entryMessage = undefined;
+
+    try {
+      const activeTab = await getActiveTab();
+
+      const authors = (await chrome.tabs.sendMessage(activeTab.id, {type: "authorRequest"})).authorList;
+
+      // If there is more than one author remove the "and " from the last one
+      if (authors.length > 1) {
+        authors[authors.length - 1] = authors[authors.length - 1].substring(4);
+      }
+
+      // Remove unneeded parts of the url and title
+      const title = activeTab.title.substring(0, activeTab.title.lastIndexOf(" - SCP Foundation"));
+      const url = activeTab.url.substring(SCP_URL.length);
+
+      console.log(document.getElementsByClassName("selected-rating")[0].innerHTML)
+      markRead("completed", url, title, authors, parseInt(document.getElementsByClassName("selected-rating")[0].innerHTML), document.getElementById("notes-box").value, Date.now());
+      
+      // Tell the user that it was successful
+      entryStatus = "successful-entry";
+
+      // TODO change message text depending on update/addition
+      // entryMessage = "Entry updated";
+      entryMessage = "Entry added";
+    } catch (e) {
+      console.log(e.message);
+      // Tell the user that it was unsuccessful, and advise reloading the page if it continues to do this
+      entryMessage = "Entry failed";
+      entryStatus = "failed-entry";
+    }
+    const notif = document.getElementById("entry-notification");
+    notif.classList.add(entryStatus);
+    notif.innerHTML = entryMessage;
 }
 
 async function getActiveTab() {
@@ -113,8 +160,10 @@ function addButtonListeners() {
     openInNewTab("../pages/options.html");
   });
 
-  document.getElementById("entry-button").addEventListener("click", async function() {
-    // Get the rating and review from the user, and the author, date, title and url from the site
+  document.getElementById("entry-button").addEventListener("click", addEntry);
+
+  document.getElementById("to-read-button").addEventListener("click", async function() {
+    // Get the review from the user, and the author, date, title and url from the site
 
     let entryStatus = undefined;
     let entryMessage = undefined;
@@ -124,6 +173,7 @@ function addButtonListeners() {
 
       const authors = (await chrome.tabs.sendMessage(activeTab.id, {type: "authorRequest"})).authorList;
 
+      // BUG: if there is only 2 authors, they will not split on the "and ". However, a single author might also have an "and " in it.
       // If there is more than one author remove the "and " from the last one
       if (authors.length > 1) {
         authors[authors.length - 1] = authors[authors.length - 1].substring(4);
@@ -134,7 +184,7 @@ function addButtonListeners() {
       const url = activeTab.url.substring(SCP_URL.length);
 
       console.log(document.getElementsByClassName("selected-rating")[0].innerHTML)
-      markRead(url, title, authors, parseInt(document.getElementsByClassName("selected-rating")[0].innerHTML), document.getElementById("notes-box").value, Date.now());
+      markRead("toRead", url, title, authors, 0, document.getElementById("notes-box").value, Date.now());
       
       // Tell the user that it was successful
       entryStatus = "successful-entry";
